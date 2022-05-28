db3e474193afab828b7866957504c24ec9f1658f
Finnbarr O'Callahan <FinnbarrOC@users.noreply.github.com>
2022-05-28 13:36:36 -0400

    Initial project files

---
+++ GoogleCalendarDiscordRichPresence/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace GoogleCalendarDiscordRichPresence
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
