30d4de5798d836aa22c476766074425aeafbb453
Finnbarr O'Callahan <FinnbarrOC@users.noreply.github.com>
2022-05-28 15:18:39 -0400

    Added authentication of the user's Google account and fetching of Calendar events

--- GoogleCalendarDiscordRichPresence/Program.cs
+++ GoogleCalendarDiscordRichPresence/Program.cs
@@ -1,12 +1,128 @@
 ﻿using System;
+using System.IO;
+using System.Threading;
+using Google.Apis.Auth.OAuth2;
+using Google.Apis.Calendar.v3;
+using Google.Apis.Calendar.v3.Data;
+using Google.Apis.Services;
+using Google.Apis.Util.Store;
 
 namespace GoogleCalendarDiscordRichPresence
 {
     class Program
     {
+        /* Global instance of the scopes required by this quickstart.
+         If modifying these scopes, delete your previously saved token.json/ folder. */
+        static string[] Scopes = { CalendarService.Scope.CalendarReadonly };
+        static string ApplicationName = "GoogleCalendarDiscordRichPresence";
+        
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            
+            // Use your client ID from Discord's developer site.
+            var clientID = Environment.GetEnvironmentVariable("DISCORD_CLIENT_ID");
+            if (clientID == null)
+            {
+                clientID = "419212778889281536";
+            }
+            
+            Console.WriteLine(clientID);
+            var discord = new Discord.Discord(Int64.Parse(clientID), (UInt64)Discord.CreateFlags.Default);
+            discord.SetLogHook(Discord.LogLevel.Debug, (level, message) =>
+            {
+                Console.WriteLine("Log[{0}] {1}", level, message);
+            });
+            
+            var activityManager = discord.GetActivityManager();
+
+            var activity = new Discord.Activity
+            {
+                State = "olleh",
+                Details = "foo details",
+                Timestamps =
+                {
+                    Start = 5,
+                    End = 6,
+                },
+                Assets =
+                {
+                    LargeImage = "foo largeImageKey",
+                    LargeText = "foo largeImageText",
+                    SmallImage = "foo smallImageKey",
+                    SmallText = "foo smallImageText",
+                },
+                Instance = true,
+            };
+            
+            Console.WriteLine(activity);
+
+            activityManager.UpdateActivity(activity, result =>
+            {
+                Console.WriteLine("Update Activity {0}", result);
+            });
+
+            GoogleCalendarMain();
+        }
+
+        static void GoogleCalendarMain()
+        {
+            try
+            {
+                UserCredential credential;
+                // Load client secrets.
+                using (var stream =
+                       new FileStream("GoogleCalendarCredentials.json", FileMode.Open, FileAccess.Read))
+                {
+                    /* The file token.json stores the user's access and refresh tokens, and is created
+                     automatically when the authorization flow completes for the first time. */
+                    string credPath = "token.json";
+                    credential = GoogleWebAuthorizationBroker.AuthorizeAsync(
+                        GoogleClientSecrets.FromStream(stream).Secrets,
+                        Scopes,
+                        "user",
+                        CancellationToken.None,
+                        new FileDataStore(credPath, true)).Result;
+                    Console.WriteLine("Credential file saved to: " + credPath);
+                }
+
+                // Create Google Calendar API service.
+                var service = new CalendarService(new BaseClientService.Initializer
+                {
+                    HttpClientInitializer = credential,
+                    ApplicationName = ApplicationName
+                });
+
+                // Define parameters of request.
+                EventsResource.ListRequest request = service.Events.List("primary");
+                request.TimeMin = DateTime.Now;
+                request.ShowDeleted = false;
+                request.SingleEvents = true;
+                request.MaxResults = 10;
+                request.OrderBy = EventsResource.ListRequest.OrderByEnum.StartTime;
+
+                // List events.
+                Events events = request.Execute();
+                Console.WriteLine("Upcoming events:");
+                if (events.Items == null || events.Items.Count == 0)
+                {
+                    Console.WriteLine("No upcoming events found.");
+                    return;
+                }
+                foreach (var eventItem in events.Items)
+                {
+                    string when = eventItem.Start.DateTime.ToString();
+                    if (String.IsNullOrEmpty(when))
+                    {
+                        when = eventItem.Start.Date;
+                    }
+                    Console.WriteLine("{0} ({1})", eventItem.Summary, when);
+                }
+            }
+            catch (FileNotFoundException e)
+            {
+                Console.WriteLine(e.Message);
+            }
         }
     }
 }
